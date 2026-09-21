0b90a25f86167d95cbb28e60f42479bc0e308bd1
Tobias <tobiasalmeidac@gmail.com>
2026-09-21 20:28:29 -0300

    inicio da estruturação dos schemas

--- app/schemas/cliente.py
+++ app/schemas/cliente.py
@@ -1,9 +1,15 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict, Field, field_validator
 from typing import Optional
 from enum import Enum
+import uuid
 
+TAMANHO_TELEFONE = 11
+TAMANHO_CPF = 11
+TAMANHO_CNPJ = 14
 
-
+class TipoDocumento(str, Enum):
+    CPF = "CPF"
+    CNPJ = "CNPJ"
 
 class StatusEnum(str, Enum):
     ATIVO = "ativo"
@@ -11,11 +17,52 @@ class StatusEnum(str, Enum):
 
 class ClienteCriar(BaseModel):
 
-    nome: str
-    cpf: str
+    model_config = ConfigDict(
+        extra="forbid"
+        json_schema_extra={
+            "exemplo"={
+                "nome": "João da silva"
+                "cpf": "11122233345"
+                "email": "exemplo@email.com"
+                "telefone": 5511911111111
+                "status": "ATIVO"
+            }
+        }
+    )
+
+    nome: str = Field(
+        min_length=1,
+        max_length=200,
+        description="nome do cliente"
+    )
+    documento: str = Field(
+        description="Documento do cliente, CPF ou CNPJ, apenas números ou pontuação"
+    )
+
+    tipoDocumento: TipoDocumento = Field(
+        description="Tipo do documento"
+    )
+
+
     email: EmailStr
-    telefone: int
-    status: StatusEnum
+    # telefone: int
+    # status: StatusEnum
+
+    @field_validator("cpf", mode="before")
+    @classmethod
+    def organizar_cpf(cls, Value: str) -> str:
+        if not isinstance(Value, str):
+            raise ValueError("CPF deve ser enviado como texto.")
+        
+        CpfApenasDigitos = "".join(c for c in Value if c.isdigit())
+
+        if len(CpfApenasDigitos) != TAMANHO_CPF:
+            raise ValueError (f"CPF deve conter {TAMANHO_CPF} digitos.")
+
+        return CpfApenasDigitos
+
+    
+
     
 class ClienteAtualizar(BaseModel):
         
